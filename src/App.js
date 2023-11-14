fba0b5bc6881dbc7904bf3ce36c8dc926393edd1
audesmus <aureliu.aurelius@gmail.com>
2023-11-14 23:53:42 +0100

    add example of updating state based on current state value.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const messages = [
 
 function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
@@ -21,29 +22,36 @@ function App() {
   // const step = 1;
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 && "active"}`}>1</div>
-        <div className={`${step >= 2 && "active"}`}>2</div>
-        <div className={`${step >= 3 && "active"}`}>3</div>
-      </div>
-      <p className="message">
-        step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          onClick={handlePrevious}
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-        >
-          previous
-        </button>
-        <button
-          onClick={handleNext}
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-        >
-          next
-        </button>
-      </div>
+    <div>
+      <button onClick={() => setIsOpen((prev) => !prev)} className="close">
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 && "active"}`}>1</div>
+            <div className={`${step >= 2 && "active"}`}>2</div>
+            <div className={`${step >= 3 && "active"}`}>3</div>
+          </div>
+          <p className="message">
+            step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              onClick={handlePrevious}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            >
+              previous
+            </button>
+            <button
+              onClick={handleNext}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            >
+              next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
